5429edf8f37be85ddc4c5edfea3a2a8efb305f95
Anthony DiRusso <ossurid@gmail.com>
2019-03-31 10:31:10 -0400

    adds db values fixes package.json typo

--- schema.sql
+++ schema.sql
@@ -1,1 +1,7 @@
 --schema.sql--
+create table Top_five_songs_about_death_a_Lauras_dad_tribute_list (
+    id serial primary key,
+    top_five_list varchar(500),
+    song varchar(200),
+    artist varchar(200)
+);
--- seed.sql
+++ seed.sql
@@ -1,1 +1,9 @@
 --seed.sql--
+insert into Top_five_songs_about_death_a_Lauras_dad_tribute_list
+    (top_five_list, song, artist)
+values
+    ('Top five songs about death: a Laura''s dad tribute list', 'Leader of the Pack', 'The Shangri-Las'),
+    ('Top five songs about death: a Laura''s dad tribute list', 'Dead Man''s Curve', 'Jan & Dean'),
+    ('Top five songs about death: a Laura''s dad tribute list', 'Tell Laura I Love Her', 'Ray Paterson'),
+    ('Top five songs about death: a Laura''s dad tribute list', 'One Step Beyond', 'Madness'),
+    ('Top five songs about death: a Laura''s dad tribute list', 'Wreck of the Edmund Fitzgerald', 'Gordon Lightfoot')
